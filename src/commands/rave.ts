7be7532746f7ecedabaeb52f09e78488d9dd9c8f
tegberen <plantpot4prez@gmail.com>
2025-12-18 15:36:23 +0100

    Update rave.ts

--- src/commands/rave.ts
+++ src/commands/rave.ts
@@ -5,6 +5,16 @@ export const data = new SlashCommandBuilder()
   .setDescription('i said ooooooo ooo ooo');
 
 export async function execute(interaction: any) {
+  const allowedChannelId = '1450171216601153566';
+  
+  if (interaction.channelId !== allowedChannelId) {
+    await interaction.reply({
+      content: 'lets /rave in the club and not here!',
+      ephemeral: true
+    });
+    return;
+  }
+
   await interaction.reply({
     content: 'https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExbWNxcXB5MW50MjN5MGhiY2sxNXl0N2p3ejFhYnQ3M3BlM2hhcHRsciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/m7URgFktt1JjDPjGKy/giphy.gif'
   });
