0b88116f5a36d2c62bf92431a236f7f2ec12a3f7
Tegberen <tegberen@gmail.com>
2025-12-18 20:26:55 +0100

    mode

--- src/commands/oracle.ts
+++ src/commands/oracle.ts
@@ -1,10 +1,17 @@
-import { SlashCommandBuilder, CommandInteraction } from 'discord.js';
+import { SlashCommandBuilder, ChatInputCommandInteraction  } from 'discord.js';
 
 export const data = new SlashCommandBuilder()
   .setName('oracle')
-  .setDescription('me olmec, you question');
+  .setDescription('me olmec, you question')
+  .addBooleanOption(option =>
+    option
+      .setName('simple')
+      .setDescription('simple yes/no mode')
+      .setRequired(false)
+  )
 
-export async function execute(interaction: CommandInteraction) {
+export async function execute(interaction: ChatInputCommandInteraction ) {
+  const simple = interaction.options.get('simple')?.value as boolean | undefined;
   const answers = [
     'Yes',
     'No',
@@ -15,7 +22,13 @@ export async function execute(interaction: CommandInteraction) {
     'This is not for me to decide'
   ];
   
-  const answer = answers[Math.floor(Math.random() * answers.length)];
+  const simpleAnswers = [
+    'Yes',
+    'No'
+  ];
+  
+  const list = simple ? simpleAnswers : answers;
+  const answer = list[Math.floor(Math.random() * answers.length)];
   
   await interaction.reply(answer);
 }
